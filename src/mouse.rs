befbc3c27ac1b91ce2a0defe773e3f6a54e12094
Michi <michael.h.25511+github@gmail.com>
2025-12-11 19:24:55 +0000

    Added an initialize function for terminal input

--- src/mouse.rs
+++ src/mouse.rs
@@ -1,5 +1,19 @@
 use std::{time::Duration, io};
-use crossterm::event::{poll, read};
+use std::io::{Write, stdout};
+use crossterm::event::{
+	EnableBracketedPaste,
+	EnableFocusChange,
+	EnableMouseCapture,
+	Event,
+	poll,
+	read,
+};
+use crossterm::{execute, style::Print};
+use crossterm::{
+	terminal::size,
+	cursor,
+	event::KeyCode
+};
 
 pub struct Mouse {
 	pub x : u16,
@@ -9,11 +23,26 @@ pub struct Mouse {
 
 impl Mouse {
 	pub fn update(&mut self) -> io::Result<()> {
-		if poll(Duration::from_millis(0))? {
+		if poll(Duration::from_millis(100))? {
 			println!("{:?}", read()?);
+			match read()? {
+				Event::FocusGained => println!("FocusGained"),
+				Event::FocusLost => println!("FocusLost"),
+				Event::Key(event) => println!("{:?}", event),
+				Event::Mouse(event) => println!("{:?}", event),
+				Event::Paste(data) => println!("Pasted {:?}", data),
+				Event::Resize(width, height) => println!("New size {}x{}", width, height),
+			}
 		}
 		Ok(())
 		// self.x = 
 		// self.y = 
 	}
+}
+
+fn init() {
+	let mut stdout = io::stdout();
+	execute!(stdout, crossterm::event::EnableBracketedPaste).unwrap();
+	execute!(stdout, crossterm::event::EnableFocusChange).unwrap();
+	execute!(stdout, crossterm::event::EnableMouseCapture).unwrap();
 }
