ff964a62765ca4c4a56e4fc087fdac5df5d454d0
Michi <michael.h.25511+github@gmail.com>
2025-12-11 19:08:39 +0000

    Tried showing inputs - Currently blocking

--- src/mouse.rs
+++ src/mouse.rs
@@ -1,16 +1,16 @@
 use std::{time::Duration, io};
-use crossterm::{event::poll};
+use crossterm::event::{poll, read};
 
-struct Mouse {
-	x : u16,
-	y : u16,
-	lclick : bool
+pub struct Mouse {
+	pub x : u16,
+	pub y : u16,
+	pub lclick : bool
 }
 
 impl Mouse {
 	pub fn update(&mut self) -> io::Result<()> {
 		if poll(Duration::from_millis(0))? {
-			println!("{:?}", crossterm::event::read()?);
+			println!("{:?}", read()?);
 		}
 		Ok(())
 		// self.x = 
