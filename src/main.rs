eb294c555c22a241d922e30f2c2501b74e00b896
Michi <michael.h.25511+github@gmail.com>
2025-12-12 16:44:18 +0000

    Changed directory structure // Added different unicode characters for UI elements

--- src/main.rs
+++ src/main.rs
@@ -11,30 +11,31 @@ use std::{
 };
 use std::{thread, time};
 
-mod input;
-mod interface;
-
+pub mod input;
+pub mod interface;
+pub mod utils;
 
 fn main() {
 	input::init().unwrap();
 	let mut mouse : input::Mouse = input::Mouse { x : 0, y : 0, lclick : false, rclick : false };
 	let mut window : input::Window = input::Window { focused : false, width : 0, height : 0 };
 
-	let mut testbox : interface::Box =
-	interface::Box {
+	let mut testbox : interface::textbox::Box =
+	interface::textbox::Box {
 		x : 10, y : 10,
-		width : 5, height : 5,
+		width : 40, height :18,
 
 		text : "".to_string(),
 		invert_on_hover : false,
 		hovered : false,
 		clicked : false,
 		rclicked : false,
+		line_type : 0,
 		color : 0
 	};
 
 	testbox.draw();
-	thread::sleep(time::Duration::from_millis(1000));
+	thread::sleep(time::Duration::from_millis(60000));
 	
 
 
--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,12 @@
+use std::io::{
+	Stdout,
+	Write,
+	stdout,
+	self,
+};
+
+// Color structs
+
 pub struct Color {
 	color : u8,
 	bright : bool
@@ -8,11 +17,10 @@ impl Color {
 		self.color = c;
 	}
 
-	pub fn write_color(&self, background : bool) -> stdout {
+	pub fn write_color(&self, background : bool) {
 		
 	}
 }
-
 
 pub struct TrueColor {
 	red : u8,
@@ -27,7 +35,40 @@ impl TrueColor {
 		self.blue = b;
 	}
 
-	pub fn write_color(&self, background : bool) -> stdout {
+	pub fn write_color(&self, background : bool) {
 		
+	}
+}
+
+
+
+// Unicode characters
+
+const NORMAL : [char; 11] =    ['─', '│', '┌', '┐', '└', '┘', '├', '┤', '┬', '┴', '┼'];
+const THICK : [char; 11] =     ['━', '┃', '┏', '┓', '┗', '┛', '┣', '┫', '┳', '┻', '╋'];
+const DOT3 : [char; 2] =       ['┄', '┆'];
+const DOT3_THICK : [char; 2] = ['┅', '┇'];
+const DOT4 : [char; 2] =       ['┈', '┊'];
+const DOT4_THICK : [char; 2] = ['┉', '┋'];
+const MLINE : [char; 9] =      ['═', '║', '╔', '╗', '╚', '╝', '╠', '╣', '╬'];
+const SHADING : [char; 6] =    ['░', '▒', '▓', '█', '▄', '▀'];
+
+pub fn repeat(out : &mut Stdout, c : char, n : u16) {
+	for _ in 0..n {
+		write!(out, "{c}").unwrap();
+	}
+}
+
+pub fn get_char(ctype : u8, index : u8) -> char {
+	match ctype {
+		0 => NORMAL[index as usize],
+		1 => THICK[index as usize],
+		2 => DOT3[index as usize],
+		3 => DOT3_THICK[index as usize],
+		4 => DOT4[index as usize],
+		5 => DOT4_THICK[index as usize],
+		6 => MLINE[index as usize],
+		7 => SHADING[index as usize],
+		_ => NORMAL[index as usize],
 	}
 }
