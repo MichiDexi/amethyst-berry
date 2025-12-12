d047f47daa42d959015cac89f4d70d04089cedd3
Michi <michael.h.25511+github@gmail.com>
2025-12-12 13:58:19 +0000

    Added right click detection to input library

--- src/main.rs
+++ src/main.rs
@@ -14,19 +14,25 @@ use std::{
 };
 
 mod input;
-mod mapdata;
 
 
 fn main() {
 	let _ = input::init();
-	let mut a : input::Mouse = input::Mouse { x : 0, y : 0, lclick : false };
+	let mut a : input::Mouse = input::Mouse { x : 0, y : 0, lclick : false, rclick : false };
 	let mut b : input::Window = input::Window { focused : false, width : 0, height : 0 };
 	loop {
 		let _ = input::update(&mut a, &mut b);
 		let mut stdout = stdout();
 		if a.lclick {
 			execute!(stdout, cursor::MoveTo(a.x, a.y)).unwrap();
-			let _ = write!(stdout, "a");
+			let _ = write!(stdout, "O");
+			if a.x == 0 && a.y == 0 {
+				break;
+			}
+		}
+		if a.rclick {
+			execute!(stdout, cursor::MoveTo(a.x, a.y)).unwrap();
+			let _ = write!(stdout, " ");
 			if a.x == 0 && a.y == 0 {
 				break;
 			}
