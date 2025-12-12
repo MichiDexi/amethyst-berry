e8f462892889c709cea8a03a26cd8bce0b23ad2e
Michi <michael.h.25511+github@gmail.com>
2025-12-12 13:48:04 +0000

    Default config options added

--- src/config.rs
+++ src/config.rs
@@ -1,28 +1,55 @@
 pub struct Program {
-	menu : MainMenuLayout,
-	list : ListLayout,
-	terminal : TerminalData
+	pub menu : MainMenuLayout,
+	pub list : ListLayout,
+	pub terminal : TerminalData,
+	pub other : Other,
 }
 
 pub struct MainMenuLayout {
-	order : Vec<String>
+	pub order : Vec<u8>
 }
 
 pub struct ListLayout {
-	name : u8,
-	index : u8,
-	value : u8,
-	date : u8,
+	pub name : u8,
+	pub index : u8,
+	pub value : u8,
+	pub date : u8,
 	
-	order : u8
+	pub order : Vec<u8>
 }
 
 pub struct TerminalData {
-	color : u8, // 0 -> no color; 1 -> 8 color; 2 -> 16 color; 3 -> 256 color; 4 -> truecolor
-	cli : bool, // false -> TUI; true -> CLI
-	mouse_enabled : bool,
+	pub color : u8, // 0 -> no color; 1 -> 8 color; 2 -> 16 color; 3 -> 256 color; 4 -> truecolor
+	pub cli : bool, // false -> TUI; true -> CLI
+	pub mouse_enabled : bool,
 }
 
 pub struct Other {
-	
+	pub custom_load_directory : bool,
+}
+
+impl Program {
+	pub fn initialize() -> Self {
+		Program {
+			MainMenuLayout {
+				order : vec!(0, 1, 2, 3, 4, 5, 6, 7, 8, 9),
+			},
+			ListLayout {
+				name : 40,
+				index : 10,
+				value : 20,
+				date : 30,
+
+				order : vec!(0, 1, 2, 3),
+			},
+			TerminalData {
+				color : 1,
+				cli : false,
+				mouse_enabled : false,
+			},
+			Other {
+				custom_load_directory : false,
+			}
+		}
+	}
 }
