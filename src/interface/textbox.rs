4827f69b879927638e9fc5e7ca0f86d4dba45baf
Michi <michael.h.25511+github@gmail.com>
2025-12-12 17:01:25 +0000

    Added custom lines to the box (and a showcase, for now)

--- src/interface/textbox.rs
+++ src/interface/textbox.rs
@@ -32,23 +32,23 @@ impl Box {
 
 		// Top
 		execute!(out, crossterm::cursor::MoveTo(self.x, self.y)).unwrap();
-		write!(out, "┌").unwrap();
-		utils::repeat(&mut out, '─', self.width-2);
-		write!(out, "┐").unwrap();
+		write!(out, "{}", utils::get_char(self.line_type, 2)).unwrap();
+		utils::repeat(&mut out, utils::get_char(self.line_type, 0), self.width-2);
+		write!(out, "{}", utils::get_char(self.line_type, 3)).unwrap();
 
 		// Middle
 		for i in 0..self.height-2 {
 			execute!(out, MoveTo(self.x, self.y + i + 1)).unwrap();
-			write!(out, "│").unwrap();
+			write!(out, "{}", utils::get_char(self.line_type, 1)).unwrap();
 			execute!(out, MoveTo(self.x + self.width - 1, self.y + i + 1)).unwrap();
-			write!(out, "│").unwrap();
+			write!(out, "{}", utils::get_char(self.line_type, 1)).unwrap();
 		}
 
 		// Bottom
 		execute!(out, MoveTo(self.x, self.y + self.height - 1)).unwrap();
-		write!(out, "└").unwrap();
-		utils::repeat(&mut out, '─', self.width-2);
-		write!(out, "┘").unwrap();
+		write!(out, "{}", utils::get_char(self.line_type, 4)).unwrap();
+		utils::repeat(&mut out, utils::get_char(self.line_type, 0), self.width-2);
+		write!(out, "{}", utils::get_char(self.line_type, 5)).unwrap();
 
 		stdout().flush().unwrap();
 	}
