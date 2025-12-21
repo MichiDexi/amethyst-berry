8c26b4baa43042b1f0158d0e1deeb95aacd42804
Michi <michael.h.25511+github@gmail.com>
2025-12-21 17:42:40 +0100

    String parsing works!

--- src/abt/mapdata.rs
+++ src/abt/mapdata.rs
@@ -1,12 +1,14 @@
-use std::path::Path;
+pub use std::path::Path;
 use std::fs;
 
+use crate::helpers;
+
 pub struct MapCluster {
-	name : String,
-	a_side : Map,
-	b_side : Map,
-	c_side : Map,
-	d_side : Map,
+	pub name : String,
+	pub a_side : Map,
+	pub b_side : Map,
+	pub c_side : Map,
+	pub d_side : Map,
 }
 
 impl MapCluster {
@@ -44,19 +46,26 @@ impl MapCluster {
 	}
 }
 
-fn parse_map(map : &String) -> Map {
+pub fn parse_map(map : &String) -> Map {
+
+	let mut map_obj_output : Map = Map::new("placeholder name");
+	let mut currentchar : u16 = helpers::utils::hex_decimal(&map[0..2]) as u16 +2;
+	// Tag string parsing
+	let tag : &str = &map[2_usize..currentchar as usize];
+	map_obj_output.tag = tag.to_string();
 	
+	map_obj_output
 }
 
 
 pub struct Checkpoint {
-	id : u8,
-	name : String,
-	notes : String,
-	chokepoints : Vec<Chokepoint>,
+	pub id : u8,
+	pub name : String,
+	pub notes : String,
+	pub chokepoints : Vec<Chokepoint>,
 
 	// Practice data
-	min_deaths : u16,
+	pub min_deaths : u16,
 }
 
 impl Checkpoint {
@@ -73,12 +82,12 @@ impl Checkpoint {
 
 
 pub struct Chokepoint {
-	id : u8,
-	name : String,
-	notes : String,
+	pub id : u8,
+	pub name : String,
+	pub notes : String,
 
 	// Practice data
-	highest_backtoback_amount : u16, // weird meassurement, but I'd use that
+	pub highest_backtoback_amount : u16, // weird meassurement, but I'd use that
 }
 
 impl Chokepoint {
@@ -94,8 +103,8 @@ impl Chokepoint {
 
 
 pub struct CheckpointRange {
-	id_start : u8,
-	id_end : u8,
+	pub id_start : u8,
+	pub id_end : u8,
 }
 
 impl CheckpointRange {
@@ -110,66 +119,66 @@ impl CheckpointRange {
 
 pub struct Map {
 	// Main data
-	name : String,
-	tag : String,
-	difficulty : u8,
-	length : u8,
-	clear_progress : u8,
-	deaths : u32,
+	pub name : String,
+	pub tag : String,
+	pub difficulty : u8,
+	pub length : u8,
+	pub clear_progress : u8,
+	pub deaths : u32,
 
 	// Strawberries
-	strawberry_amount : u16,
-	strawberry_collected : u16,
+	pub strawberry_amount : u16,
+	pub strawberry_collected : u16,
 	
 	// Golden berries refer to 'main-deathlessberries', the type can be changed
-	goldberry_exists : bool,
-	goldberry_collected : bool,
-	goldberry_type : u8,
+	pub goldberry_exists : bool,
+	pub goldberry_collected : bool,
+	pub goldberry_type : u8,
 	
 	// Silver berries refer to 'sub-deathlessberries', the type can be changed
-	silverberry_exists : bool,
-	silverberry_amount : u8,
-	silverberry_collected : u8,
-	silverberry_type : u8,
+	pub silverberry_exists : bool,
+	pub silverberry_amount : u8,
+	pub silverberry_collected : u8,
+	pub silverberry_type : u8,
 
 	// This is a place for the 'Moonberry' or 'Winged Goldberry'
-	specialberry_exists : bool,
-	specialberry_name : String,
-	specialberry_collected : bool,
+	pub specialberry_exists : bool,
+	pub specialberry_name : String,
+	pub specialberry_collected : bool,
 
 	// Challenge data
-	challenge_activate : bool,
-	min_dashes_pb : u16,
-	min_dashes_bronze : u16,
-	min_dashes_silver : u16,
-	min_dashes_gold : u16,
-	min_jumps_pb : u16,
-	min_jumps_bronze : u16,
-	min_jumps_silver : u16,
-	min_jumps_gold : u16,
+	pub challenge_activate : bool,
+	pub min_dashes_pb : u16,
+	pub min_dashes_bronze : u16,
+	pub min_dashes_silver : u16,
+	pub min_dashes_gold : u16,
+	pub min_jumps_pb : u16,
+	pub min_jumps_bronze : u16,
+	pub min_jumps_silver : u16,
+	pub min_jumps_gold : u16,
 
 	// Speedrun data
-	fastest_time : u64,
-	speedrun_bronze : u64,
-	speedrun_silver : u64,
-	speedrun_gold : u64,
+	pub fastest_time : u64,
+	pub speedrun_bronze : u64,
+	pub speedrun_silver : u64,
+	pub speedrun_gold : u64,
 	
 	// Extra data no one could care less about
-	cassette_exists : bool,
-	cassette_collected : bool,
-	crystalheart_exists : bool,
-	crystalheart_name : String,
-	crystalheart_collected : bool,
+	pub cassette_exists : bool,
+	pub cassette_collected : bool,
+	pub crystalheart_exists : bool,
+	pub crystalheart_name : String,
+	pub crystalheart_collected : bool,
 
 
 	// Here is all the practice data
 	// This is very complex data, that may or may not break when I'll try to parse it
 
 
-	pb : u32, // Furthest the user has come
-	min_deaths_pb : u32,
-	checkpoints : Vec<Checkpoint>,
-	range_runs : Vec<CheckpointRange>,
+	pub pb : u32, // Furthest the user has come
+	pub min_deaths_pb : u32,
+	pub checkpoints : Vec<Checkpoint>,
+	pub range_runs : Vec<CheckpointRange>,
 }
 
 impl Map {
