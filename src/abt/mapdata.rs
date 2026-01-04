f594b803798c003e2ac0a618e735864373c19b82
Michi <michael.h.25511+github@gmail.com>
2026-01-04 14:10:26 +0100

    New parser

--- src/abt/mapdata.rs
+++ src/abt/mapdata.rs
@@ -3,88 +3,76 @@ use std::fs;
 
 use crate::abt::mapparsing::*;
 
-pub struct MapCluster {
-	pub name : String,
-	pub a_side : Map,
-	pub b_side : Map,
-	pub c_side : Map,
-	pub d_side : Map,
-}
-
-impl MapCluster {
-	pub fn new(nname : &str) -> Self {
-		Self {
-			name : nname.to_string(),
-			a_side : Map::new(&(nname.to_string() + " A-side")),
-			b_side : Map::new(&(nname.to_string() + " B-side")),
-			c_side : Map::new(&(nname.to_string() + " C-side")),
-			d_side : Map::new(&(nname.to_string() + " D-side")),
-		}
-	}
-
-	pub fn load(&mut self, path : &Path) {
-		let mapcluster : Vec<String> = fs::read_to_string(path)
-			.unwrap()
-			.lines()
-			.map(|line| line.to_string())
-			.collect();
-
-		for (i, map) in (0_u8..).zip(mapcluster.into_iter()) {
-			let map_obj : Map = parse_map(&map);
-			match i {
-				0 => { self.a_side = map_obj; },
-				1 => { self.b_side = map_obj; },
-				2 => { self.c_side = map_obj; },
-				3 => { self.d_side = map_obj; },
-				_ => {}
-			};
-		}
-	}
-
-	pub fn save(path : &Path) {
-		
-	}
-}
-
 pub struct Checkpoint {
-	pub id : u8,
 	pub name : String,
-	pub notes : String,
+	pub notes : Notes,
 	pub chokepoints : Vec<Chokepoint>,
+	pub silver_collected : Option<bool>,
 
 	// Practice data
 	pub min_deaths : u16,
+	pub runs_died : u16,
+	pub runs_passed : u16,
 }
 
 impl Checkpoint {
-	pub fn new(nname : &str, nid : u8) -> Self {
+	pub fn new(nname : &str, silvers_exist : bool) -> Self {
 		Self {
-			id : nid,
 			name : nname.to_string(),
-			notes : "".to_string(),
+			notes : Notes::new(),
 			chokepoints : Vec::new(),
-			min_deaths : 0
+			silver_collected : match silvers_exist {
+				true => Some(false),
+				false => None
+			},
+			
+			min_deaths : 0,
+			runs_died : 0,
+			runs_passed : 0,
 		}
 	}
 }
 
 
 pub struct Chokepoint {
-	pub id : u8,
 	pub name : String,
-	pub notes : String,
+	pub notes : Notes,
 
 	// Practice data
 	pub highest_backtoback_amount : u16, // weird meassurement, but I'd use that
+	pub runs_died : u16,
+	pub runs_passed : u16,
 }
 
 impl Chokepoint {
-	pub fn new(nname : &str, nid : u8) -> Self {
+	pub fn new(nname : &str) -> Self {
 		Self {
-			id : nid,
 			name : nname.to_string(),
+			notes : Notes::new(),
+			
+			highest_backtoback_amount : 0,
+			runs_died : 0,
+			runs_passed : 0,
+		}
+	}
+}
+
+pub struct Notes {
+	pub notes : String,
+	pub speedrun_notes : String,
+	pub min_dash_notes : String,
+	pub min_jump_notes : String,
+	pub goldenberry_notes : String,
+}
+
+impl Notes {
+	fn new() -> Self {
+		Self {
 			notes : "".to_string(),
-			highest_backtoback_amount : 0
+			speedrun_notes : "".to_string(),
+			min_dash_notes : "".to_string(),
+			min_jump_notes : "".to_string(),
+			goldenberry_notes : "".to_string(),
 		}
 	}
 }
@@ -96,11 +84,15 @@ pub struct CheckpointRange {
 }
 
 impl CheckpointRange {
-	pub fn new(nid_s : u8, nid_e : u8) -> Self {
-		Self {
+	pub fn new(nid_s : u8, nid_e : u8) -> Option<Self> {
+		if nid_s >= nid_e {
+			return None;
+		}
+	
+		Some(Self {
 			id_start : nid_s,
 			id_end : nid_e
-		}
+		})
 	}
 }
 
@@ -108,104 +100,92 @@ impl CheckpointRange {
 pub struct Map {
 	// Main data
 	pub name : String,
-	pub tag : String,
+	pub starred : bool,
+	pub rating : Option<u8>,
+	pub tags : Vec<String>,
 	pub difficulty : u8,
-	pub clear_progress : u8,
+	pub clear_progress : Option<u8>,
 	pub deaths : u32,
 
-	// Strawberries
-	pub strawberry_amount : u16,
-	pub strawberry_collected : u16,
-	
-	// Golden berries refer to 'main-deathlessberries', the type can be changed
-	pub goldberry_exists : bool,
-	pub goldberry_collected : bool,
-	pub goldberry_type : u8,
-	
-	// Silver berries refer to 'sub-deathlessberries', the type can be changed
-	pub silverberry_exists : bool,
-	pub silverberry_amount : u8,
-	pub silverberry_collected : u8,
-	pub silverberry_type : u8,
+	// Berries
+	pub strawberry : Counted,
+	pub goldberry : Option<TypedCollectable>,
+	pub silverberries : Option<u8>, // type
 
 	// This is a place for the 'Moonberry' or 'Winged Goldberry'
-	pub specialberry_exists : bool,
-	pub specialberry_name : String,
-	pub specialberry_collected : bool,
+	pub specialberry : Option<NamedCollectable>,
 
-	// Challenge data
-	pub challenge_activate : bool,
-	pub min_dashes_pb : u16,
-	pub min_dashes_bronze : u16,
-	pub min_dashes_silver : u16,
-	pub min_dashes_gold : u16,
-	pub min_jumps_pb : u16,
-	pub min_jumps_bronze : u16,
-	pub min_jumps_silver : u16,
-	pub min_jumps_gold : u16,
-
-	// Speedrun data
-	pub fastest_time : u64,
-	pub speedrun_bronze : u64,
-	pub speedrun_silver : u64,
-	pub speedrun_gold : u64,
+	// Extra
+	pub cassette : Option<bool>,
+	pub crystalheart : Option<NamedCollectable>,
 	
-	// Extra data no one could care less about
-	pub cassette_exists : bool,
-	pub cassette_collected : bool,
-	pub crystalheart_exists : bool,
-	pub crystalheart_name : String,
-	pub crystalheart_collected : bool,
+	// Challenge data
+	pub min_dashes : Option<Tiered<u16>>,
+	pub min_jumps : Option<Tiered<u16>>,
+	pub speedrun : Option<Tiered<u64>>,
 
-
-	// Here is all the practice data
-	// This is very complex data, that may or may not break when I'll try to parse it
-
-
-	pub pb : u32, // Furthest the user has come
+	// Practice data
+	pub pb : u32,
 	pub min_deaths_pb : u32,
 	pub checkpoints : Vec<Checkpoint>,
 	pub range_runs : Vec<CheckpointRange>,
 }
 
+pub struct Counted {
+	pub collected : u16,
+	pub total : u16,
+}
+
+pub struct Tiered<T> {
+	pub pb : T,
+	pub bronze : T,
+	pub silver : T,
+	pub gold : T,
+}
+
+pub struct NamedCollectable {
+	pub collected : bool,
+	pub name : String
+}
+
+pub struct TypedCollectable {
+	pub collected : bool,
+	pub btype : u8
+}
+
+
 impl Map {
-	pub fn new(nname : &str) -> Self {
+	pub fn new() -> Self {
 		Self {
-			name : nname.to_string(),
-			tag : "".to_string(),
+			name : "Unnamed".to_string(),
+			starred : false,
+			rating : None,
+			tags : Vec::new(),
 			difficulty : 0,
-			clear_progress : 0,
+			clear_progress : None,
 			deaths : 0,
-			strawberry_amount : 0,
-			strawberry_collected : 0,
-			goldberry_exists : false,
-			goldberry_collected : false,
-			goldberry_type : 0,
-			silverberry_exists : false,
-			silverberry_amount : 0,
-			silverberry_collected : 0,
-			silverberry_type : 0,
-			specialberry_exists : false,
-			specialberry_name : "".to_string(),
-			specialberry_collected : false,
-			challenge_activate : false,
-			min_dashes_pb : 0,
-			min_dashes_bronze : 0,
-			min_dashes_silver : 0,
-			min_dashes_gold : 0,
-			min_jumps_pb : 0,
-			min_jumps_bronze : 0,
-			min_jumps_silver : 0,
-			min_jumps_gold : 0,
-			fastest_time : 0,
-			speedrun_bronze : 0,
-			speedrun_silver : 0,
-			speedrun_gold : 0,
-			cassette_exists : false,
-			cassette_collected : false,
-			crystalheart_exists : false,
-			crystalheart_name : "".to_string(),
-			crystalheart_collected : false,
+
+			// Berries
+			strawberry : Counted {
+				collected : 0,
+				total : 0,
+			},
+			goldberry : None,
+			silverberries : None, // type
+
+			// This is a place for the 'Moonberry' or 'Winged Goldberry'
+			specialberry : None,
+
+			// Extra
+			cassette : None,
+			crystalheart : None,
+			
+			// Challenge data
+			min_dashes : None,
+			min_jumps : None,
+			speedrun : None,
+
+			// Practice data
 			pb : 0,
 			min_deaths_pb : 0,
 			checkpoints : Vec::new(),
@@ -215,25 +195,54 @@ impl Map {
 }
 
 pub fn present_map(map : &Map) {
-	println!("map {} - {} - {} - {} - {}", map.name, map.tag, map.difficulty, map.clear_progress, map.deaths);
-	println!("str {} - {}", map.strawberry_amount, map.strawberry_collected);
-	println!("gol {} - {} - {}", map.goldberry_exists, map.goldberry_collected, map.goldberry_type);
-	println!("sil {} - {} - {} - {}", map.silverberry_exists, map.silverberry_amount, map.silverberry_collected, map.silverberry_type);
-	println!("spe {} - {} - {}", map.specialberry_exists, map.specialberry_name, map.specialberry_collected);
-	println!("cha {} - {} - {} - {} - {} - {} - {} - {} - {}", map.challenge_activate, map.min_dashes_pb, map.min_dashes_bronze, map.min_dashes_silver, map.min_dashes_gold, map.min_jumps_pb, map.min_jumps_bronze, map.min_jumps_silver, map.min_jumps_gold);
-	println!("frn {} - {} - {} - {}", map.fastest_time, map.speedrun_bronze, map.speedrun_silver, map.speedrun_gold);
-	println!("cry {} - {} - {} - {} - {}", map.cassette_exists, map.cassette_collected, map.crystalheart_exists, map.crystalheart_name, map.crystalheart_collected);
-	println!("pra {} - {} - {} - {} - {}", map.pb, map.min_deaths_pb, map.difficulty, map.clear_progress, map.deaths);
+	println!("name: {}", map.name);
+	println!("starred: {}", map.starred);
+	if let Some(v) = &map.rating {
+		println!("rating: {}", v);
+	}
+	for tag in &map.tags {
+		println!("tag: {}", tag);
+	}
+	println!("difficulty: {}", map.difficulty);
+	if let Some(v) = &map.clear_progress {
+		println!("clear_progress: {}", v);
+	}
+	println!("deaths: {}", map.deaths);
 
+	println!("strawberry c: {}", map.strawberry.collected);
+	println!("strawberry t: {}", map.strawberry.total);
+	if let Some(v) = &map.goldberry {
+		println!("goldberry c: {}", v.collected);
+		println!("goldberry t: {}", v.btype);
+	}
+	if let Some(v) = &map.silverberries {
+		println!("silver t: {}", v);
+	}
+
+	if let Some(v) = &map.specialberry {
+		println!("special c: {}", v.collected);
+		println!("special n: {}", v.name);
+	}
+
+	if let Some(v) = &map.cassette {
+		println!("cassette c: {}", v);
+	}
+
+	if let Some(v) = &map.crystalheart {
+		println!("crystalheart c: {}", v.collected);
+		println!("crystalheart n: {}", v.name);
+	}
+
+	if let Some(v) = &map.min_dashes {
+		println!("min_dashes: {} {} {} {}", v.pb, v.bronze, v.silver, v.gold);
+	}
+	if let Some(v) = &map.min_jumps {
+		println!("min_jumps: {} {} {} {}", v.pb, v.bronze, v.silver, v.gold);
+	}
+	if let Some(v) = &map.speedrun {
+		println!("speedrun: {} {} {} {}", v.pb, v.bronze, v.silver, v.gold);
+	}
 	
-	for checkpoint in &map.checkpoints {
-		println!("  che {} - {} - {} - {}", checkpoint.name, checkpoint.id, checkpoint.notes, checkpoint.min_deaths);
-		for chokepoint in &checkpoint.chokepoints {
-			println!("    cho {} - {} - {} - {}", chokepoint.name, chokepoint.id, chokepoint.notes, chokepoint.highest_backtoback_amount);
-		}
-	}
-
-	for range in &map.range_runs {
-		println!("  ran {} - {}", range.id_start, range.id_end);
-	}
+	println!("pb: {}", map.pb);
+	println!("min_deaths_pb: {}", map.min_deaths_pb);
 }
--- src/main.rs
+++ src/main.rs
@@ -21,9 +21,143 @@ pub mod helpers;
 pub mod menus;
 pub mod abt;
 
-const TARGET_FPS : f64 = 30.0;
+const TARGET_FPS : f64 = 60.0;
 
 fn main() -> io::Result<()> {
+
+	let abyss : abt::mapdata::Map = abt::mapdata::Map {
+		name : "Abyss".to_string(),
+		starred : false,
+		rating : Some(100),
+		tags : vec!("my favorite map".to_string()),
+		difficulty : 7,
+		clear_progress : Some(2),
+		deaths : 1214,
+
+		strawberry : abt::mapdata::Counted {
+			collected : 11,
+			total : 11,
+		},
+		goldberry : Some(abt::mapdata::TypedCollectable {
+			collected : true,
+			btype : 1,
+		}),
+		silverberries : None,
+
+		specialberry : None,
+
+		cassette : None,
+		crystalheart : None,
+
+		min_dashes : None,
+		min_jumps : None,
+		speedrun : None,
+
+		pb : 0,
+		min_deaths_pb : 314,
+		checkpoints : vec!(
+			abt::mapdata::Checkpoint {
+				name : "Start".to_string(),
+				notes : abt::mapdata::Notes {
+					notes : "".to_string(),
+					speedrun_notes : "".to_string(),
+					min_dash_notes : "".to_string(),
+					min_jump_notes : "".to_string(),
+					goldenberry_notes : "".to_string(),
+				},
+				chokepoints : vec!(),
+				silver_collected : None,
+
+				min_deaths : 2,
+				runs_died : 2,
+				runs_passed : 0
+			},
+			abt::mapdata::Checkpoint {
+				name : "Sunset".to_string(),
+				notes : abt::mapdata::Notes {
+					notes : "".to_string(),
+					speedrun_notes : "".to_string(),
+					min_dash_notes : "".to_string(),
+					min_jump_notes : "".to_string(),
+					goldenberry_notes : "".to_string(),
+				},
+				chokepoints : vec!(),
+				silver_collected : None,
+
+				min_deaths : 2,
+				runs_died : 2,
+				runs_passed : 0
+			},
+			abt::mapdata::Checkpoint {
+				name : "Countryside".to_string(),
+				notes : abt::mapdata::Notes {
+					notes : "".to_string(),
+					speedrun_notes : "".to_string(),
+					min_dash_notes : "".to_string(),
+					min_jump_notes : "".to_string(),
+					goldenberry_notes : "".to_string(),
+				},
+				chokepoints : vec!(),
+				silver_collected : None,
+
+				min_deaths : 2,
+				runs_died : 2,
+				runs_passed : 0
+			},
+			abt::mapdata::Checkpoint {
+				name : "City".to_string(),
+				notes : abt::mapdata::Notes {
+					notes : "".to_string(),
+					speedrun_notes : "".to_string(),
+					min_dash_notes : "".to_string(),
+					min_jump_notes : "".to_string(),
+					goldenberry_notes : "".to_string(),
+				},
+				chokepoints : vec!(),
+				silver_collected : None,
+
+				min_deaths : 2,
+				runs_died : 2,
+				runs_passed : 0
+			},
+			abt::mapdata::Checkpoint {
+				name : "Abyss".to_string(),
+				notes : abt::mapdata::Notes {
+					notes : "".to_string(),
+					speedrun_notes : "".to_string(),
+					min_dash_notes : "".to_string(),
+					min_jump_notes : "".to_string(),
+					goldenberry_notes : "".to_string(),
+				},
+				chokepoints : vec!(),
+				silver_collected : None,
+
+				min_deaths : 2,
+				runs_died : 2,
+				runs_passed : 0
+			}
+		),
+		range_runs : vec!(
+			abt::mapdata::CheckpointRange {
+				id_start : 0,
+				id_end : 1,
+			}
+		)
+	};
+	println!("{}\n\n", abt::mapparsing::parse_map_as_string(abyss));
+
+
+	let map = abt::mapparsing::parse_map_as_string(abt::mapdata::Map::new());
+	abt::mapdata::present_map(&abt::mapdata::Map::new());
+	println!("{}\n\n", map);
+
+	if let Some(v) = abt::mapparsing::parse_string_as_map(abt::mapparsing::parse_map_as_string(abt::mapdata::Map::new())) {
+		abt::mapdata::present_map(&v);
+	}
+	
+
+	return Ok(());
+
 	// Initialize everything, including:
 	// InputHandler, Main menu
 	helpers::input::init()?;
